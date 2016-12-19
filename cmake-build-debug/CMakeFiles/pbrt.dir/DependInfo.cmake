d936e6a3489235291e0f85c1b83d273aed4a9e45
eyesfrog <eyesfrog@outlook.com>
2016-12-18 22:31:24 -0700

    pbrt v3(add template)

---
+++ cmake-build-debug/CMakeFiles/pbrt.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/eyesfrog/ClionProjects/pbrt/src/core/geometry.cpp" "/Users/eyesfrog/ClionProjects/pbrt/cmake-build-debug/CMakeFiles/pbrt.dir/src/core/geometry.cpp.o"
+  "/Users/eyesfrog/ClionProjects/pbrt/src/core/scene.cpp" "/Users/eyesfrog/ClionProjects/pbrt/cmake-build-debug/CMakeFiles/pbrt.dir/src/core/scene.cpp.o"
+  "/Users/eyesfrog/ClionProjects/pbrt/src/main/pbrt.cpp" "/Users/eyesfrog/ClionProjects/pbrt/cmake-build-debug/CMakeFiles/pbrt.dir/src/main/pbrt.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/pbrt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pbrt.dir/src/main/pbrt.cpp.o"
+  "CMakeFiles/pbrt.dir/src/core/scene.cpp.o"
+  "CMakeFiles/pbrt.dir/src/core/geometry.cpp.o"
+  "pbrt.pdb"
+  "pbrt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pbrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
