dd4288bab99a88c2b48aadb4315d68bbdbc3ba43
eyesfrog <eyesfrog@outlook.com>
2016-12-24 23:21:15 -0700

    rename

---
+++ cmake-build-debug/CMakeFiles/TinyRay.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/BRDF/BRDF.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/BRDF/BRDF.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/BRDF/GlossySpecular.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/BRDF/GlossySpecular.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/BRDF/Lambertian.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/BRDF/Lambertian.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/BRDF/PerfectSpecular.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/BRDF/PerfectSpecular.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Build/Build.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Build/Build.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Camera/Camera.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Camera/Camera.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Camera/Fisheye.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Camera/Fisheye.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Camera/Pinhole.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Camera/Pinhole.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/GeometricObjects/GeometricObject.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/GeometricObjects/GeometricObject.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/GeometricObjects/Plane.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/GeometricObjects/Plane.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/GeometricObjects/Rectangle.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/GeometricObjects/Rectangle.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/GeometricObjects/Sphere.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/GeometricObjects/Sphere.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Lights/Ambient.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Lights/Ambient.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Lights/AreaLight.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Lights/AreaLight.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Lights/Directional.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Lights/Directional.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Lights/Light.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Lights/Light.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Lights/PointLight.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Lights/PointLight.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Materials/Emissive.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Materials/Emissive.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Materials/Material.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Materials/Material.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Materials/Matte.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Materials/Matte.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Materials/Phong.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Materials/Phong.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Materials/Reflective.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Materials/Reflective.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Sampler/MultiJittered.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Sampler/MultiJittered.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Sampler/Regular.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Sampler/Regular.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Sampler/Sampler.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Sampler/Sampler.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Tracer/AreaLighting.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Tracer/AreaLighting.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Tracer/MultipleObjects.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Tracer/MultipleObjects.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Tracer/RayCast.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Tracer/RayCast.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Tracer/SingleSphere.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Tracer/SingleSphere.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Tracer/Tracer.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Tracer/Tracer.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Tracer/Whitted.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Tracer/Whitted.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/BBox.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/BBox.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Math.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Math.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Matrix.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Matrix.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Normal.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Normal.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Point2D.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Point2D.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Point3D.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Point3D.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/RGBColor.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/RGBColor.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Ray.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Ray.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/ShadeRec.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/ShadeRec.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/Utilities/Vector3D.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/Utilities/Vector3D.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/World/ViewPlane.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/World/ViewPlane.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/World/World.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/World/World.cpp.o"
+  "/Users/eyesfrog/ClionProjects/TinyRay/src/main.cpp" "/Users/eyesfrog/ClionProjects/TinyRay/cmake-build-debug/CMakeFiles/TinyRay.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
